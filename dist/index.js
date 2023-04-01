1a8204c12d753228dc9d40d5e393c3451812fa64
ABDULLAH-SALEEM <79781595+ABDULLAH-SALEEM@users.noreply.github.com>
2023-04-01 13:33:27 +0500

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,54 +1,36 @@
 "use strict";
-let apiUrl = "https://rickandmortyapi.com/api/character";
-const table = document.createElement("table");
-const tableBody = document.createElement("tbody");
-const headerData = ['ID', 'NAME', 'STATUS', 'SPECIES', 'TYPE', 'GENDER', 'IMAGE'];
-let characterData = [];
-function generateTable() {
-    const table = document.createElement("table");
-    const tableBody = document.createElement("tbody");
-    const header = document.createElement("tr");
-    headerData.map((data) => {
-        const cell = document.createElement("th");
-        const cellText = document.createTextNode(data);
-        cell.appendChild(cellText);
-        header.appendChild(cell);
+const getUsers = async () => {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.error(error);
+    }
+};
+const createTable = (data) => {
+    const table = document.createElement('table');
+    const header = table.createTHead();
+    const row = header.insertRow();
+    row.insertCell().innerText = 'ID';
+    row.insertCell().innerText = 'Name';
+    row.insertCell().innerText = 'Username';
+    row.insertCell().innerText = 'Email';
+    const body = table.createTBody();
+    data === null || data === void 0 ? void 0 : data.forEach(user => {
+        const row = body.insertRow();
+        row.insertCell().innerText = user.id.toString();
+        row.insertCell().innerText = user.name;
+        row.insertCell().innerText = user.username;
+        row.insertCell().innerText = user.email;
     });
-    tableBody.appendChild(header);
-    characterData.map((data, idx) => {
-        const row = document.createElement("tr");
-        Object.values(data).map((tableData, idx) => {
-            tableData = tableData.toString();
-            const cell = document.createElement("td");
-            if (!tableData) {
-                tableData = 'N/A';
-            }
-            if (tableData.includes('https')) {
-                var img = document.createElement('img');
-                img.src = tableData;
-                cell.appendChild(img);
-            }
-            else {
-                const cellText = document.createTextNode(tableData);
-                cell.appendChild(cellText);
-            }
-            row.appendChild(cell);
-        });
-        tableBody.appendChild(row);
-    });
-    table.appendChild(tableBody);
+    return table;
+};
+const main = async () => {
+    const users = await getUsers();
+    const table = createTable(users);
     document.body.appendChild(table);
-}
-fetch(apiUrl).then(function (response) {
-    response === null || response === void 0 ? void 0 : response.json().then((data) => {
-        data.results.map(({ id, name, status, species, type, gender, image }, idx) => {
-            characterData.push({
-                id, name, status, species, type, gender, image
-            });
-        });
-        generateTable();
-    });
-}).catch(function (err) {
-    console.log('Something went wrong.', err);
-});
+};
+main();
 //# sourceMappingURL=index.js.map
